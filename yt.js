58bebd6391ec8a3f17a5e7f15827f1a6e6df3203
Mosabbir Masud <95482155+mosabbirx@users.noreply.github.com>
2022-01-04 00:53:27 +0600

    Update yt.js

--- yt.js
+++ yt.js
@@ -10,7 +10,7 @@
         data-loop="1"
         data-playlist="PLAYLIST-LEAVE EMPTY IF ONLY SINGLE SONG"             
         id="youtube-audio">
-        <script src="https://www.youtube.com/iframe_api"></script>
+        <script src="https:"></script>
         <script src="https://rawcdn.githack.com/Mr-Kranarong/Hidden-Youtube-Background-Music-JS/0d0d34dbb33614e1c99f85ad9899e2397f17770b/yt.js"></script>
  </div>
 */
